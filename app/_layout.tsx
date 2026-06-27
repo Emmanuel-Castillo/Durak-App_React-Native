7d2b3bd18a1e8b225a1a3df59554f19157b7396b
Emmanuel-Castillo <emmanuel.2001alonso.ec@gmail.com>
2026-06-26 20:05:49 -0500

    Removed notification initialization

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,4 @@
 import { useAuthStore } from "@/store/auth.store";
-import { User } from "@/type";
-import { registerForPushNotificationsAsync } from "@/utils/notif";
-import { updatePushToken } from "@/utils/supabase";
 import { Stack } from "expo-router";
 import React, { useEffect } from "react";
 import "./global.css";
@@ -13,21 +10,6 @@ const RootLayout = () => {
   useEffect(() => {
     fetchAuthenticatedUser();
   }, []);
-
-  useEffect(() => {
-    const setupNotifications = async (user: User) => {
-      try {
-        // console.log("[setupNotifications]: Register for push notifications");
-        const token = await registerForPushNotificationsAsync();
-        if (token) await updatePushToken(user, token);
-      } catch (e: any) {
-        console.log(e);
-        // Alert.alert(e.toString());
-      }
-    };
-    user && !isAnonymous && setupNotifications(user); // Setup notifications ONLY for non-anonymous users!!!
-  }, [user]);
-
   if (isLoading) return null;
 
   return <Stack screenOptions={{ headerShown: false }}></Stack>;
